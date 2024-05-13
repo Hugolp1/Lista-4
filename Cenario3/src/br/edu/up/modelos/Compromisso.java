1ef317b09ff41f7988b308611f121e9be693a16b
unknown <hugolourencoprado@gmail.com>
2024-05-13 15:17:38 -0300

    Classes Cenario 3

---
+++ Cenario3/src/br/edu/up/modelos/Compromisso.java
@@ -0,0 +1,37 @@
+package br.edu.up.modelos;
+
+public class Compromisso {
+    private String pessoa;
+    private String local;
+    private int hora;
+    private int dia;
+    private int mes;
+
+    public Compromisso(String pessoa, String local, int hora, int dia, int mes) {
+        this.pessoa = pessoa;
+        this.local = local;
+        this.hora = hora;
+        this.dia = dia;
+        this.mes = mes;
+    }
+
+    public String getPessoa() {
+        return pessoa;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+}
