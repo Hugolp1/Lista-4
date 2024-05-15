d25afb6e9e8f3736f49bfa3f8844207400d06c95
Victor Anhaya <victoranhaya8@icloud.com>
2024-05-15 10:53:53 -0300

    Cenario 7 Completo

--- Cenario6/src/br/edu/up/Programa.java
+++ Cenario6/src/br/edu/up/Programa.java
@@ -1,6 +1,12 @@
 package br.edu.up;
+
+import br.edu.up.controller.AeroportoController;
+import br.edu.up.view.AeroportoView;
+
 public class Programa {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        AeroportoView view = new AeroportoView();
+        AeroportoController controller = new AeroportoController(view);
+        controller.iniciar();
     }
 }
