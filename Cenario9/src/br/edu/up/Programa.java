bcec5c92a4cc960d273be4ae2c928f9f9f6fbda8
Victor Anhaya <victoranhaya8@icloud.com>
2024-05-15 10:58:52 -0300

    Cenario 10 Completo

--- Cenario9/src/br/edu/up/Programa.java
+++ Cenario9/src/br/edu/up/Programa.java
@@ -1,6 +1,12 @@
 package br.edu.up;
+
+import br.edu.up.controller.ControleAcademicoController;
+import br.edu.up.view.ControleAcademicoView;
+
 public class Programa {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        ControleAcademicoView view = new ControleAcademicoView();
+        ControleAcademicoController controller = new ControleAcademicoController(view);
+        controller.iniciar();
     }
 }
