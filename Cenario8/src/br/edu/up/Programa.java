3a9aad39a378e5438f3d0a55137d7ad5c275baeb
Victor Anhaya <victoranhaya8@icloud.com>
2024-05-17 10:07:14 -0300

    Cenario 8 Completo

--- Cenario8/src/br/edu/up/Programa.java
+++ Cenario8/src/br/edu/up/Programa.java
@@ -1,12 +1,9 @@
 package br.edu.up;
 
-import br.edu.up.controller.ControleAcademicoController;
-import br.edu.up.view.ControleAcademicoView;
-
 public class Programa {
     public static void main(String[] args) {
         ControleAcademicoView view = new ControleAcademicoView();
-        ControleAcademicoController controller = new ControleAcademicoController();
+        ControleAcademicoController controller = new ControleAcademicoController(view);
         controller.iniciar();
     }
 }
